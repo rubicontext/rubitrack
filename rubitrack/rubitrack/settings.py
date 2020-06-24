91e20998215cd1c768917598fa719c910ba26c00
rubicontext <you@example.com>
2020-06-24 14:02:08 +0630

    import of collection OK - TODO refresh currently playing

--- rubitrack/rubitrack/settings.py
+++ rubitrack/rubitrack/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['lula', 'http://193.70.86.101/', '127.0.0.1']
 
 INSTALLED_APPS = [
     'track.apps.TrackConfig',
+    #'adminsortable2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'rubitrack.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
