f3e4a789efc6aa53d0f7dff692e98edf718d9e8b
rubicontext <antoine.carnet@rubicontext.com>
2020-10-23 06:22:08 +0200

    add transition without refresh + reduce title size

--- rubitrack/track/transition.py
+++ rubitrack/track/transition.py
@@ -1,19 +1,21 @@
 from django.http import HttpResponse
 from .models import Transition, TransitionType, Track
-from .currently_playing import display_currently_playing
+from .currently_playing import display_currently_playing, get_more_transition_before_block
 
 #ADD NEW TRANSITION
 def add_new_transition(request):
-	trackId = request.GET['trackId']
-	destinationId = request.GET['destinationId']
+	trackSourceId = request.GET['trackSourceId']
+	trackDestinationId = request.GET['trackDestinationId']
 	transition=Transition()
-	transition.track_source=Track.objects.get(id=trackId)
-	transition.track_destination=Track.objects.get(id=destinationId)
+	transition.track_source=Track.objects.get(id=trackSourceId)
+	transition.track_destination=Track.objects.get(id=trackDestinationId)
 	transition.comment='Added automatically'
 	transition.transition_type=TransitionType.objects.get(id=1)
 	transition.save()
+	#return(display_currently_playing(request))
 
-	return(display_currently_playing(request))
+	#return the previous transition block for display
+	return(get_more_transition_before_block(request))
 
 #DELETE TRANSITION
 def delete_transition(request):
