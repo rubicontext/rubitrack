c7789cd0cf5906b2c4f83c1b8a1ca6aec26239b4
rubicontext <antoine.carnet@rubicontext.com>
2020-11-02 16:56:28 +0100

    add missing template for blank refresh

---
+++ rubitrack/scripts/discogs_api.py
@@ -0,0 +1,7 @@
+import discogs_api
+
+#Consumer Key	wfwzcctShOfnAoZVlOwp
+#Consumer Secret	NUlHtGuxyMfTVOaCqWmhfcyctUmnSkYu
+#Demander l'URL du Token	https://api.discogs.com/oauth/request_token
+#Autoriser l'URL	https://www.discogs.com/fr/oauth/authorize
+#URL du Jeton D'Accès	https://api.discogs.com/oauth/access_token
